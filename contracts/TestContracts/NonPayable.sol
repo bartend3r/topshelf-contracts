1212896b7c232b6e9229c1488f44ca4c00ecd52f
mix0logist <88318920+mix0logist@users.noreply.github.com>
2021-09-27 20:52:28 +0200

    Add collateralToken for testing

--- contracts/TestContracts/NonPayable.sol
+++ contracts/TestContracts/NonPayable.sol
@@ -2,11 +2,13 @@
 
 pragma solidity 0.6.11;
 
-//import "../Dependencies/console.sol";
+// import "../Dependencies/console.sol";
+import "../Dependencies/IERC20.sol";
 
 
 contract NonPayable {
     bool isPayable;
+    IERC20 public collateralToken;
 
     function setPayable(bool _isPayable) external {
         isPayable = _isPayable;
@@ -14,7 +16,7 @@ contract NonPayable {
 
     function forward(address _dest, bytes calldata _data) external payable {
         (bool success, bytes memory returnData) = _dest.call{ value: msg.value }(_data);
-        //console.logBytes(returnData);
+        // console.logBytes(returnData);
         require(success, string(returnData));
     }
 
