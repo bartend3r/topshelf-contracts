ebacaff25f528f84979f2a864b5151f7d94fe8c3
mix0logist <88318920+mix0logist@users.noreply.github.com>
2021-10-17 17:30:57 +0200

    Remove test for method that has been removed

--- test/GrowthTokenTest.js
+++ test/GrowthTokenTest.js
@@ -253,7 +253,7 @@ contract('LQTY Token', async accounts => {
 
     await assertRevert(lqtyTokenTester.transfer(lqtyTokenTester.address, 1, { from: A }))
     await assertRevert(lqtyTokenTester.transfer(ZERO_ADDRESS, 1, { from: A }))
-    await assertRevert(lqtyTokenTester.transfer(communityIssuance.address, 1, { from: A }))
+    // await assertRevert(lqtyTokenTester.transfer(communityIssuance.address, 1, { from: A }))
     // tranfers to staking contract with transfer are allowed.
     // await assertRevert(lqtyTokenTester.transfer(lqtyStaking.address, 1, { from: A }))
   })
