2077c9fa282dc1bab251d6b44ef1caf88039fdb7
bartend3r <bartend3r@protonmail.com>
2021-12-05 22:57:38 +0400

    add latestAnswer to chainlink interface

--- contracts/Dependencies/AggregatorV3Interface.sol
+++ contracts/Dependencies/AggregatorV3Interface.sol
@@ -8,6 +8,7 @@ interface AggregatorV3Interface {
   function decimals() external view returns (uint8);
   function description() external view returns (string memory);
   function version() external view returns (uint256);
+  function latestAnswer() external view returns (int256);
 
   // getRoundData and latestRoundData should both raise "No data present"
   // if they do not have data to report, instead of returning unset values
