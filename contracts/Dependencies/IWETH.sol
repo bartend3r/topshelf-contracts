eb218821ea307491a510468282b83f86d16c50e3
bartend3r <bartend3r@protonmail.com>
2021-12-09 21:35:57 +0400

    weth delegator

--- contracts/Dependencies/IWETH.sol
+++ contracts/Dependencies/IWETH.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.6.11;
 
 interface IWETH {
+    function balanceOf(address account) external view returns (uint256);
     function deposit() external payable;
     function withdraw(uint256) external;
     function approve(address guy, uint256 wad) external returns (bool);
