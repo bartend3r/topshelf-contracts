6baf89eccc2b61ac1b8d206ccb1d46d465a489e6
bartend3r <bartend3r@protonmail.com>
2021-12-16 03:29:00 +0400

    incentive gauge

---
+++ contracts/LQTY/IncentiveGauge.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.11;
+
+import "../Dependencies/IERC20.sol";
+import "../Interfaces/ICommunityIssuance.sol";
+import "../Dependencies/Ownable.sol";
+import "../Dependencies/SafeMath.sol";
+
+
+contract IncentiveGauge is Ownable {
+    using SafeMath for uint256;
+
+    IERC20 public lqtyToken;
+    ICommunityIssuance public communityIssuance;
+
+    uint256 issued;
+    uint256 available;
+
+    constructor(address _lqtyTokenAddress, address _communityIssuanceAddress) public Ownable() {
+        lqtyToken = IERC20(_lqtyTokenAddress);
+        communityIssuance = ICommunityIssuance(_communityIssuanceAddress);
+    }
+
+    function updateAvailable() external {
+        uint256 issuance = communityIssuance.issueLQTY();
+        available = available.add(issuance);
+    }
+
+    function sendTokens(address _account, uint _amount) external onlyOwner {
+        uint256 issuance = communityIssuance.issueLQTY();
+        available = available.add(issuance);
+        available = available.sub(_amount, "Exceeds available amount");
+        communityIssuance.sendLQTY(_account, _amount);
+        issued = issued.add(_amount);
+    }
+
+}
